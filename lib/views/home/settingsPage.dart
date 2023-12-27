2c5b878d0e117e56df0fafab34c3f77dae1e51e5
Karlmathuthu <karlmathuthu11@gmail.com>
2023-12-27 14:00:44 +0200

    updated

--- lib/views/home/settingsPage.dart
+++ lib/views/home/settingsPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:settings_ui/settings_ui.dart';
 import 'package:vision_craft_mobile/theme/appTheme.dart';
 import 'package:vision_craft_mobile/views/home/webview.dart';
 
@@ -15,6 +15,13 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  void openWebview({required String urlString, required String title}) {
+    Get.to(
+      () => WebsiteView(urlString: urlString, title: title),
+      transition: Transition.cupertino,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,126 +36,182 @@ class _SettingsPageState extends State<SettingsPage> {
             ],
           ),
         ),
-        child: SettingsList(
-          lightTheme: const SettingsThemeData(
-            settingsListBackground: Colors.transparent,
-          ),
-          physics: const BouncingScrollPhysics(),
-          applicationType: ApplicationType.material,
-          contentPadding: EdgeInsets.zero,
-          brightness: Brightness.light,
-          platform: DevicePlatform.iOS,
-          sections: [
-            //support
-            SettingsSection(
-              title: Text(
-                'Support',
-                style: GoogleFonts.lato(
-                  color: Colors.white,
-                ),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(90, 35, 35, 65),
+                borderRadius: BorderRadius.circular(10),
               ),
-              tiles: <SettingsTile>[
-                SettingsTile.navigation(
-                  onPressed: (context) {
-                    Get.to(
-                      () => WebsiteView(
-                        urlString: AppConstants.support,
-                        title: "How-Tos",
+              child: Column(
+                children: [
+                  const SizedBox(height: 15),
+
+                  GestureDetector(
+                    onTap: () {
+                      openWebview(
+                          title: "Support", urlString: AppConstants.support);
+                    },
+                    child: SizedBox(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 10),
+                          const Icon(
+                            Icons.star_outline,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            "Support",
+                            style: GoogleFonts.josefinSans(color: Colors.white),
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                  trailing: SizedBox(),
-                  title: const Text(
-                    'How-Tos',
-                    style: TextStyle(
-                      fontFamily: "SF",
-                      fontSize: 15,
                     ),
                   ),
-                ),
-                SettingsTile.navigation(
-                  onPressed: (context) {
-                    Get.to(
-                      () => WebsiteView(
-                        urlString: AppConstants.offcialSite,
-                        title: "LPG Level Sensor Documentation",
-                      ),
-                    );
-                  },
-                  trailing: SizedBox(),
-                  title: const Text(
-                    'LPG Level Sensor Documentation',
-                    style: TextStyle(
-                      fontFamily: "SF",
-                      fontSize: 15,
+                  const Padding(
+                    padding: EdgeInsets.only(left: 40, right: 10),
+                    child: Divider(
+                      color: Colors.white12,
+                      height: 1,
                     ),
                   ),
-                ),
-                SettingsTile.navigation(
-                  onPressed: (context) {
-                    Get.to(
-                      () => WebsiteView(
-                        urlString: AppConstants.offcialSite,
-                        title: "Privacy Policy",
+                  const SizedBox(height: 10),
+                  //share with firends
+                  GestureDetector(
+                    onTap: () {
+                      openWebview(urlString: AppConstants.help, title: "Help");
+                    },
+                    child: SizedBox(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 10),
+                          const Icon(
+                            Icons.share_outlined,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            "Get Help",
+                            style: GoogleFonts.josefinSans(color: Colors.white),
+                          ),
+                        ],
                       ),
-                    );
-                  },
-                  trailing: SizedBox(),
-                  title: const Text(
-                    'Privacy Policy',
-                    style: TextStyle(
-                      fontFamily: "SF",
-                      fontSize: 15,
                     ),
                   ),
-                ),
-                SettingsTile.navigation(
-                  onPressed: (context) {
-                    Get.to(
-                      () => WebsiteView(
-                        urlString: AppConstants.owner,
-                        title: "FAQs",
-                      ),
-                    );
-                  },
-                  trailing: SizedBox(),
-                  title: const Text(
-                    'FAQs',
-                    style: TextStyle(
-                      fontFamily: "SF",
-                      fontSize: 15,
+
+                  const Padding(
+                    padding: EdgeInsets.only(left: 40, right: 10),
+                    child: Divider(
+                      color: Colors.white12,
+                      height: 1,
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 10),
+                  //feedback
+                  GestureDetector(
+                    onTap: () {
+                      openWebview(
+                          urlString: AppConstants.about, title: "About Us");
+                    },
+                    child: SizedBox(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 10),
+                          const Icon(
+                            Icons.share_outlined,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            "About Us",
+                            style: GoogleFonts.josefinSans(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 40, right: 10),
+                    child: Divider(
+                      color: Colors.white12,
+                      height: 1,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  //Privacy policy
+                  GestureDetector(
+                    onTap: () {
+                      openWebview(
+                          urlString: AppConstants.offcialSite,
+                          title: "Official Site");
+                    },
+                    child: SizedBox(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 10),
+                          const Icon(
+                            Icons.privacy_tip_rounded,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            "Official Site",
+                            style: GoogleFonts.josefinSans(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 40, right: 10),
+                    child: Divider(
+                      color: Colors.white12,
+                      height: 1,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  //Privacy policy
+                  GestureDetector(
+                    onTap: () {
+                      openWebview(
+                          urlString: AppConstants.owner, title: "Owners");
+                    },
+                    child: SizedBox(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          const SizedBox(width: 10),
+                          const Icon(
+                            Icons.privacy_tip_rounded,
+                            color: Colors.white,
+                          ),
+                          const SizedBox(width: 10),
+                          Text(
+                            "Owners",
+                            style: GoogleFonts.josefinSans(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ),
             ),
-            SettingsSection(
-              title: Text(
-                'More Information',
-                style: GoogleFonts.lato(
-                  color: Colors.white,
-                ),
-              ),
-              tiles: <SettingsTile>[
-                SettingsTile.navigation(
-                  onPressed: (context) {
-                    Get.to(
-                      () => WebsiteView(
-                        urlString: AppConstants.about,
-                        title: "More information",
-                      ),
-                    );
-                  },
-                  trailing: SizedBox(),
-                  title: const Text(
-                    'About Us',
-                    style: TextStyle(
-                      fontFamily: "SF",
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ],
+            const SizedBox(height: 30),
+            //version text.
+            Text(
+              "Version 1.0.0",
+              style: GoogleFonts.josefinSans(color: Colors.white54),
             ),
           ],
         ),
